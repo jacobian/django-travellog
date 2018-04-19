1712cbeebb8ccceb620757ada3927c545793b7b8
Jacob Kaplan-Moss <jacob@jacobian.org>
2018-04-19 10:40:17 -0700

    Fooling with nested inlines. Not sure I love it, but we'll see.

--- travellog/admin.py
+++ travellog/admin.py
@@ -1,23 +1,34 @@
 from django.contrib import admin
+import nested_admin
 from . import models as tl_models
 from . import forms as tl_forms
 
+class PhotoInline(nested_admin.NestedTabularInline):
+    model = tl_models.Photo
+    extra = 0
+
+class LocationInline(nested_admin.NestedStackedInline):
+    model = tl_models.Location
+    form = tl_forms.LocationForm
+    fields = (
+        ('title', 'date'),
+        'text',
+        ('latitude', 'longitude', 'zoom', 'bearing', 'pitch'),
+    )
+    extra = 0
+    inlines = [PhotoInline]
+
+class SectionInline(nested_admin.NestedStackedInline):
+    model = tl_models.Section
+    form = tl_forms.SectionForm
+    fields = (('title', 'icon'),)
+    extra = 0
+    inlines = [LocationInline]
+
 @admin.register(tl_models.TravelLog)
-class TravelLogAdmin(admin.ModelAdmin):
+class TravelLogAdmin(nested_admin.NestedModelAdmin):
     prepopulated_fields = {"slug": ["title"]}
     form = tl_forms.TravelLogForm
-
-@admin.register(tl_models.Section)
-class SectionAdmin(admin.ModelAdmin):
-    form = tl_forms.SectionForm
-
-class PhotoInline(admin.TabularInline):
-    model = tl_models.Photo
-    extra = 1
-
-@admin.register(tl_models.Location)
-class LocationAdmin(admin.ModelAdmin):
-    form = tl_forms.LocationForm
-    inlines = [PhotoInline]
+    inlines = [SectionInline]
 
 
--- travellog/urls.py
+++ travellog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('<slug:slug>/', views.travellog),
     path('admin/', admin.site.urls),
+    path('nested_admin/', include('nested_admin.urls')),
 ]
